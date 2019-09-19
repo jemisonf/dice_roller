4555753608e42175523b063c65e0d79d45191a2c
Fischer Jemison <jemisonf@oregonstate.edu>
2019-09-19 11:03:55 -0700

    adds gulp build

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const { src, dest, parallel } = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify')
+const csso = require('gulp-csso');
+const htmlmin = require('gulp-htmlmin');
+
+html = (cb) => {
+    return src('index.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('dist'));
+}
+
+js = (cb) => {
+    return src('index.js')
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(uglify())
+        .pipe(dest('dist'));
+}
+
+css = (cb) => {
+    return src('index.css')
+        .pipe(csso())
+        .pipe(dest('dist'))
+}
+
+exports.default = parallel(js, css, html);
